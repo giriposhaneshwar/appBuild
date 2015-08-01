504eba47040c7ce618eea65887ff42edef4b60c1
giriposhaneshwar <egp.designs@gmail.com>
2015-08-01 17:38:14 +0530

    Configuration in server is modifyed

--- server/config.php
+++ server/config.php
@@ -6,8 +6,8 @@
 		define("DB_TYPE", "mysql");
 		define("DB_HOST", "localhost");
 		define("DB_USER", "root");
-		define("DB_PASS", "mysql");
-		define("DB_NAME", "appBuild");
+		define("DB_PASS", "");
+		define("DB_NAME", "appbuild");
 
 	}else{
 		// netword credentaials
