177bed931770c1d9fcf3811ba3f0e7193bd00659
giriposhaneshwar <egp.designs@gmail.com>
2015-08-25 08:07:18 +0530

    cm controller is changed

--- appBuild/controllers/cmController.js
+++ appBuild/controllers/cmController.js
@@ -40,7 +40,27 @@
     // Adding customer to database
     $scope.addCustomer = function (d, data) {
       d.preventDefault()
-      console.log('Adding Customer is ', d, data)
+      console.log('Adding Customer is ', d, data, $scope.dcReport)
+
+
+       $scope.dataObject.data = $scope.dcReport
+       $scope.dcReport.data.res = data;
+      $scope.dataObject.method = 'customers/insertCustomer'
+
+      var dt = serviceCall.getService($scope.dataObject, function (data) {
+        $scope.msg = data
+        console.log('data', data)
+        // {status: "Success", message: "Data added successfully"}
+
+        if(data.status == 'success') {
+          alert(data.message)
+          $scope.formCancel(d, data)
+        } else {
+          // Handle the errors here
+        }
+      })
+
+
     }
   }
 
