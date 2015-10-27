7a2063ddc77631300b7a1be2677f2e891e7834cc
giriposhaneshwar <egp.designs@gmail.com>
2015-10-27 10:30:09 +0530

    for Safe Development this css file was commented and working with restclient

--- server/libs/Bootstrap.php
+++ server/libs/Bootstrap.php
@@ -14,6 +14,7 @@ class Bootstrap
 	
 	function init(){
 		$this->getUrl();
+
 		
 		if(!empty($this->_url[0])){
 			$this->_loadController($this->_url[0], $this->_url[1], $this->_data);
@@ -21,6 +22,7 @@ class Bootstrap
 	}
 
 	function getUrl(){
+		mysqli_set_charset($con, "utf8");
 		$jsonData = file_get_contents("php://input");
 
 		if(get_magic_quotes_gpc()){
@@ -31,7 +33,44 @@ class Bootstrap
 
 		$d = json_decode($d,true);
 
-		// var_dump($d);
+
+
+
+
+
+		// $k=preg_replace('/\s+/', '',$d);
+		// $k = str_replace('&quot;', '"', $d);
+		// $de = json_decode( preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $k), true );
+
+		/*switch (json_last_error()) {
+	        case JSON_ERROR_NONE:
+	            echo ' - No errors';
+	        break;
+	        case JSON_ERROR_DEPTH:
+	            echo ' - Maximum stack depth exceeded';
+	        break;
+	        case JSON_ERROR_STATE_MISMATCH:
+	            echo ' - Underflow or the modes mismatch';
+	        break;
+	        case JSON_ERROR_CTRL_CHAR:
+	            echo ' - Unexpected control character found';
+	        break;
+	        case JSON_ERROR_SYNTAX:
+	            echo ' - Syntax error, malformed JSON';
+	        break;
+	        case JSON_ERROR_UTF8:
+	            echo ' - Malformed UTF-8 characters, possibly incorrectly encoded';
+	        break;
+	        default:
+	            echo ' - Unknown error';
+	        break;
+	    }*/
+
+
+
+
+
+		
 
 		$url = $d['method'];
         $url = rtrim($url, '/');
