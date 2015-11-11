893ef6105d72e36f64fcebf273365d7d15d8ff38
giriposhaneshwar <egp.designs@gmail.com>
2015-11-11 16:53:56 +0530

    Product Management - Listing part is done

--- appBuild/controllers/pmController.js
+++ appBuild/controllers/pmController.js
@@ -1,28 +1,45 @@
-(function () {
+(function() {
   // 'use strict'
   var app = angular.module('appBuild')
 
-  var pmController = function ($scope, sTest, $http, serviceCall, $location) {
-    // $scope.msg = "Page loaded successfully"
-    $scope.msg = 'PM Page'
-    // main controller page function
-    $scope.page()
-    // $scope.msg = $http.get(url)
-    $scope.curNav = $location.path().substring(1)
-    console.log('page', $scope.curNav)
+  var pmController = function($scope, sTest, $http, serviceCall, $location) {
+      // $scope.msg = "Page loaded successfully"
+      $scope.msg = 'PM Page'
+        // main controller page function
+      $scope.page()
+        // $scope.msg = $http.get(url)
+      $scope.curNav = $location.path().substring(1)
+      console.log('page', $scope.curNav)
 
-    // Get List of Customers
+      // Get List of Customers
+      $scope.dataObject.method = 'products/getStats'
+      $scope.dataObject.data.res = {}
+      console.log('Sending object ', $scope.dataObject)
+      var dt = serviceCall.getService($scope.dataObject, function(res) {
+        $scope.msg = res
+      });
 
-    $scope.dataObject.method = 'products/getStats'
-    $scope.dataObject.data.res = {}
+      // Default Activating the Products List
+      $scope.tabActive = 'productsList'
 
-    console.log('Sending object ', $scope.dataObject)
+      // Adding Product
+      $scope.addProductTab = function() {
+        $scope.tabActive = 'addProduct';
+      }
 
-    var dt = serviceCall.getService($scope.dataObject, function (res) {
-      $scope.msg = res
-    })
+      // Listing the Products
+      $scope.productsListTab = function() {
+        $scope.tabActive = 'productsList';
+        $scope.pList = appData.products;
+      }
 
-  }
+      if ($scope.tabActive == 'productsList') {
+        $scope.productsListTab()
+      }
+
+
+    } // End Variable "pmContainer"
+
 
   // Defining the controller
   app.controller('pmController', pmController)
