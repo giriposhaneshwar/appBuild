456d3774f29b13e4076ffc824b6011054b952e84
giriposhaneshwar <egp.designs@gmail.com>
2016-02-04 00:22:24 +0530

    routing server path changed

--- appBuild/appData.js
+++ appBuild/appData.js
@@ -5,9 +5,9 @@ var appData = {
 }
 
 // Local
-// var serverCon = 'http://localhost/appbuild/server/';
+ var serverCon = 'http://localhost/appbuild/server/';
 // var serverCon = 'http://tirumalatrading.com/server/';
 
 // Netrowk
-var serverCon = 'http://icsitpark.in/appbuild/server/';
+//var serverCon = 'http://icsitpark.in/appbuild/server/';
 // var serverCon = 'http://dc.icsitpark.in/';
